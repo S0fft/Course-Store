db4b02bf88c03efe4ba29033c1e7b6cd8e43b0eb
Yevhen <pelikh123.evg@gmail.com>
2023-05-08 21:53:43 +0200

    api_upgrade

--- base/settings.py
+++ base/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'shop.apps.ShopConfig'
-    'api.apps.ApiConfig'
+    'shop.apps.ShopConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
